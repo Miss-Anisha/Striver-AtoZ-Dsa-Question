dca2d7f1dbdd45845be77e19a8b350af61feb923
Miss-Anisha <115165828+Miss-Anisha@users.noreply.github.com>
2024-01-31 22:46:42 +0530

    Update Duplicate.java

--- Array/Duplicate.java
+++ Array/Duplicate.java
@@ -3,7 +3,7 @@ class Solution {
         int temp=1;
         for(int i=1; i<nums.length; i++){
             if(nums[i] != nums[i-1]){
-                nums[temp] = nums[i];
+                nums[temp] = nums[i]; //fot leet code 
                 temp++;
             }
         }
