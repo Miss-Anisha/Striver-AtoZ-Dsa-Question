d7a278b1145e3dc94faf30b2186f49df2ae9231f
Miss-Anisha <115165828+Miss-Anisha@users.noreply.github.com>
2024-02-01 12:22:13 +0530

    Update Tiiling.java

--- Recursion/Tiiling.java
+++ Recursion/Tiiling.java
@@ -1,1 +1,14 @@
-
+public class Tilingrecursion {
+    public static int tilingRe(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        int ver=tilingRe(n-1);
+        int hor=tilingRe(n-2);
+        int totalways=ver+hor;
+        return totalways;
+    }
+    public static void main(String[] args) {
+        System.out.println((tilingRe(3)));
+    }
+}
