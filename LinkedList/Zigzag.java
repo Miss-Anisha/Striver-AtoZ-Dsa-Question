d42cffa8251328e0bdad4b1538c88dfc50f018b9
Miss-Anisha <115165828+Miss-Anisha@users.noreply.github.com>
2024-02-21 21:19:30 +0530

    Update Zigzag.java

--- LinkedList/Zigzag.java
+++ LinkedList/Zigzag.java
@@ -1,1 +1,95 @@
+import java.util.LinkedList;
+public class Zigzag {
+    public static class Node{
+        int data;
+        Node next;
 
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int  size;
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void print(){
+        if(head==null){
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    
+    //zigzag
+    public void zigZag(){
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse
+        Node curr = mid.next;
+        mid.next=null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next=curr.next;
+            curr.next= prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+
+        //alt merge - zig-zag merge
+        while(left != null && right!=null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left=nextL;
+            right=nextR;
+        }
+    }
+    public static void main(String[] args) {
+        Zigzag ll = new Zigzag();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        ll.print();
+        ll.zigZag();
+        ll.print();
+    }
+}
+//output is:
+//1->2->3->4->5->6->null
+//1->6->2->5->3->4->null
