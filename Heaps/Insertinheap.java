3362a0a8fc90cfe537ac58ef20e51f8a68b92786
Miss-Anisha <115165828+Miss-Anisha@users.noreply.github.com>
2024-04-07 23:46:15 +0530

    Update Insertinheap.java

--- Heaps/Insertinheap.java
+++ Heaps/Insertinheap.java
@@ -1,1 +1,20 @@
+//only function
+public class Insertinheap {
+    static class Heap{//final complexity is o(logn)
+        ArrayList<Integer> arr= new ArrayList<>();
 
+        public void add(int data){
+            //add at last idx
+            arr.add(data);//o(1)
+
+            int x=arr.size()-1;//x is child idx
+            int par=(x-1)/2;//parent idx
+
+            while(arr.get(x)<arr.get(par)){//logn
+                //swap
+                int temp=arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par,temp);
+            }
+        }
+    }
