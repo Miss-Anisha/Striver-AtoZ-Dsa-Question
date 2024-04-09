f2ee1b4784991df14029a9657263bc1d0e0a850e
Miss-Anisha <115165828+Miss-Anisha@users.noreply.github.com>
2024-04-09 22:35:43 +0530

    Update Hashmap.java

--- Hashmap/Hashmap.java
+++ Hashmap/Hashmap.java
@@ -1,1 +1,38 @@
+import java.util.*;
+public class Hashmap {
+
+    public static void main(String[] args) {
+        //create
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //Insert   tc-O(1)
+        hm.put("India", 200);
+        hm.put("China", 100);
+        hm.put("US", 150);
+
+        System.out.println(hm);
+
+        //Get operation  tc-O(1)
+        int population=hm.get("India");
+        System.out.println(population);
+
+        //contains key tc-O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("Indonasia"));
+
+        //remove tc-O(1)
+        System.out.println(hm.remove("India"));
+        System.out.println(hm);
+
+        //size
+        System.out.println(hm.size());
+
+        //Isempty
+        System.out.println(hm.isEmpty());
+
+        //clear
+        hm.clear();
+        System.out.println(hm.isEmpty());
+    }
+}
 
